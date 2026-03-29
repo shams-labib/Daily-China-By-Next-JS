49fdd8af63452c83d9045e59a488f96f4dc0574f
shams al labib <samslabib19@gmail.com>
2026-03-29 18:57:47 +0600

    navbar added

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,7 +1,29 @@
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
-  return <div>this is header</div>;
+  return (
+    <div className="max-w-7xl">
+      <nav className="flex justify-between items-center">
+        {/* Home */}
+        <div>
+          <h1>Daily China</h1>
+        </div>
+        {/* Links */}
+        <ul>
+          <Link href={"/register"} className="font-bold text-xl text-white">
+            Register
+          </Link>
+          <Link href={"/login"} className="font-bold text-xl text-white">
+            Login
+          </Link>
+          <Link href={"/usd"} className="font-bold text-xl text-white">
+            USD
+          </Link>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/Components/Header";
+import { ThemeProvider } from "next-themes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +31,9 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         <Header />
-        {children}
+        <ThemeProvider defaultTheme="light" attribute="class">
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
